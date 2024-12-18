5959d2194d9127c3f013124b0326e363787a9ab6
gabrielbarth <gabriel.silverio@stone.com.br>
2024-12-17 22:29:37 -0400

    chore: set app status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,15 @@
+import { StatusBar } from "expo-status-bar";
 import { Stack } from "expo-router";
+import { useTheme } from "@/hooks/useTheme";
 
 export default function Layout() {
+  const { theme } = useTheme();
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <StatusBar backgroundColor={theme.background} />
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </>
   );
 }
